a31cd821afc5bf9acb60ae05a4cbe6d8b0bce5f1
murrmaaa <umamurochka@gmail.com>
2026-04-29 02:01:30 +0300

    Add T3: RECTS and SAME commands (variant 14)

---
+++ murzina.uma/T3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+#include <cmath>
+#include <map>
+#include <cstdlib>
+
+using namespace std::placeholders;
+
+int main(int argc, char* argv[]) {
+    std::cout << "T3: RECTS and SAME commands (variant 14)" << std::endl;
+    return EXIT_SUCCESS;
+}
